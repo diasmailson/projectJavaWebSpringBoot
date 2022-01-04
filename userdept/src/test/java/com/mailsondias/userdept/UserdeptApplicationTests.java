e9922edb1842f23712058766ae8cf83dc65b863f
mailsondias7 <mailsondias7@gmail.com>
2022-01-04 16:10:29 -0300

    criacao do projeto

---
+++ userdept/src/test/java/com/mailsondias/userdept/UserdeptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mailsondias.userdept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserdeptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
